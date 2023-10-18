99f1b32e5c083211e59856f7b58f59949aa0da4f
bholmesdev <bholmesdev@gmail.com>
2023-10-18 12:06:36 -0400

    feat: set default heroImage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
-import sitemap from "@astrojs/sitemap"
-import { defineConfig, field } from "studio-private-beta"
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, field } from "studio-private-beta";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [sitemap()],
-	studio: {
-		collections: {
-			Blog: {
-				fields: {
-					title: field.text(),
-					description: field.text(),
-					slug: field.text({ optional: true }),
-					publishedAt: field.date({ default: "now" }),
-					updatedAt: field.date({ optional: true }),
-					heroImage: field.text({ optional: true }),
-					content: field.text({ multiline: true }),
-				},
-			},
-		},
-	},
-})
+  site: "https://example.com",
+  integrations: [sitemap()],
+  studio: {
+    collections: {
+      Blog: {
+        fields: {
+          title: field.text(),
+          description: field.text(),
+          slug: field.text({ optional: true }),
+          publishedAt: field.date({ default: "now" }),
+          updatedAt: field.date({ optional: true }),
+          heroImage: field.text({
+            default: "/blog-placeholder-1.jpg",
+          }),
+          content: field.text({ multiline: true }),
+        },
+      },
+    },
+  },
+});
